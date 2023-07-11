c71bbdf27b726d13966abfecbb905e596db9bd67
Borstolus <115802442+Borstolus@users.noreply.github.com>
2023-07-11 17:11:55 +0200

    color changed

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const menuLinks = document.querySelector('.header-menu');
-  
-    hamburgerMenu.addEventListener('click', () => {
-      menuLinks.classList.toggle('active-menu');
-    });
-  });
