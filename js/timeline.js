1de1201c527cc6d35a77916eef631504d4140f7c
Sebastian Hitziger <115802442+Borstolus@users.noreply.github.com>
2023-07-19 22:52:22 +0200

    .

---
+++ js/timeline.js
@@ -0,0 +1,4 @@
+function toggleTimelineVisibility() {
+    const timeline = document.querySelector('.timeline');
+    timeline.style.display = timeline.style.display === 'none' ? 'grid' : 'none';
+}
