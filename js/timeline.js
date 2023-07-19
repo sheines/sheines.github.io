604702beebf33cbad07db25b3dd4a3d0a0dd5ec3
Sebastian Hitziger <115802442+Borstolus@users.noreply.github.com>
2023-07-19 23:07:55 +0200

    Delete timeline.js

--- js/timeline.js
+++
@@ -1,4 +0,0 @@
-function toggleTimelineVisibility() {
-    const timeline = document.querySelector('.timeline');
-    timeline.style.display = timeline.style.display === 'none' ? 'grid' : 'none';
-}
