291385874188dc8ef93a26c8dae7f8907e78fd19
Sebastian Hitziger <115802442+Borstolus@users.noreply.github.com>
2023-07-20 18:24:20 +0200

    changed date added

--- js/lastModified.js
+++ js/lastModified.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Function to get the last modified date of the current page
-    function getLastModifiedDate() {
-        const lastModified = new Date(document.lastModified);
-        return lastModified.toISOString().split('T')[0]; // Format as YYYY-MM-DD
-    }
+  // Function to get the current date and format it as "5. Dezember 2023"
+  function getCurrentDate() {
+    const currentDate = new Date();
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    return currentDate.toLocaleDateString('de-DE', options);
+  }
 
-    // Function to update the footer element with the last modified date
-    function updateLastModifiedDate() {
-        const lastModifiedFooter = document.getElementById("lastModifiedFooter");
-        if (lastModifiedFooter) {
-        lastModifiedFooter.innerText = "Last modified: " + getLastModifiedDate();
-        }
+  // Function to update the last modified date in the footer
+  function updateLastModifiedDate() {
+    const lastModifiedDateSpan = document.getElementById("lastModifiedDate");
+    if (lastModifiedDateSpan) {
+      lastModifiedDateSpan.textContent = getCurrentDate();
     }
+  }
 
-    // Call the function once the page is loaded
-    updateLastModifiedDate();
-    });
+  // Call the function when the page loads
+  updateLastModifiedDate();
+});
+      
