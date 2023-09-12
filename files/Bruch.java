4364ae9a7ea237135539dd104be69b3d17a54009
Borstolus <115802442+shitziger@users.noreply.github.com>
2023-09-12 17:29:13 +0200

    Create Bruch.java

---
+++ files/Bruch.java
@@ -0,0 +1,24 @@
+public class Bruch {
+    
+    int zaehler;
+    int nenner; 
+
+    public void kuerzen() {
+        int h = groessterGemeinsamerTeiler(zaehler, nenner);
+    }
+    private int groessterGemeinsamerTeiler(int a, int b) {
+            
+            if (a == 0) return Math.abs(b);
+            if (b == 0) return Math.abs(a);
+
+            int h;
+            do
+            {
+                h = a % b;
+                a = b;
+                b = h;
+            } while (b != 0);
+
+            return Math.abs(a);
+    }
+}
