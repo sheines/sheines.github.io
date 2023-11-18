2138ca9a0a03bfb5c11b447a28b8e83806d84025
Sebastian Hitziger <115802442+shitziger@users.noreply.github.com>
2023-11-18 12:45:30 +0100

    quiztest added

---
+++ js/quiz.js
@@ -0,0 +1,31 @@
+function submitQuiz() {
+    const questions = document.querySelectorAll('.question');
+
+    const totalQuestions = questions.length;
+
+    let correctCount = 0;
+    let correctAnswers = '';
+
+    for (let i = 0; i < totalQuestions; i++) {
+        const selectedOption = questions[i].querySelector('input:checked');
+
+        if (selectedOption) {
+            const answer = selectedOption.value;
+            if (i === 0 && (answer === 'Berlin' || answer === 'Oslo')) {
+                correctCount++;
+                correctAnswers += '<p>Frage 1: Richtig!</p>';
+                document.getElementById("q1").style.color = "green";
+            } else if (i === 1 && (answer === 'Seine')) {
+                correctCount++;
+                correctAnswers += '<p>Frage 2: Richtig!</p>';
+            }
+        }
+        const resultContainer = document.getElementById("result");
+        const scoreContainer = document.getElementById("score");
+        const correctAnswersContainer = document.getElementById("correct-answers");
+
+        scoreContainer.innerHTML = `<p>Du hast ${correctCount} von ${totalQuestions} richtig beantwortet.</p>`;
+        correctAnswersContainer.innerHTML = correctAnswers;
+        resultContainer.style.display = 'block';
+    }
+}
