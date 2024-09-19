542e0293a85f6aa556f83febd51a9c72a8575625
Sebastian Hitziger <115802442+shitziger@users.noreply.github.com>
2024-09-19 19:14:28 +0200

    übungen added

---
+++ js/uebungen.js
@@ -0,0 +1,111 @@
+/* Enthält:
+
+Natürliche Zahlen
+    - Addition
+    - Subtraktion
+    - Multiplikation
+    - Division
+
+
+
+*/
+
+// Zahlen
+let a = 0;
+let b = 0;
+let c = 0;
+
+
+// Natürliche Zahlen
+const natadd = document.getElementById("natadd");
+const nataddL = document.getElementById("nataddL");
+const natsub = document.getElementById("natsub");
+const natsubL = document.getElementById("natsubL");
+const natmul = document.getElementById("natmul");
+const natmulL = document.getElementById("natmulL");
+const natdiv = document.getElementById("natdiv");
+const natvivL = document.getElementById("natdivL");
+
+natadd.addEventListener('click', () => {
+    a = randomNumber(0,100);
+    b = randomNumber(0,100);
+
+    c = a+b;
+
+    natadd.innerHTML = "\\(" + a + " + " + b + "=\\)";
+    nataddL.innerHTML = "\\(\\," + c + "\\)";
+
+    
+    MathJax.typeset();
+});
+
+natsub.addEventListener('click', () => {
+    a = randomNumber(0,100);
+    b = randomNumber(0,100);
+
+    while (b > a) {
+        a = randomNumber(0,100);
+        b = randomNumber(0,100);
+    }
+
+    c = a-b;
+
+    natsub.innerHTML = "\\(" + a + " - " + b + "=\\)";
+    natsubL.innerHTML = "\\(\\," + c + "\\)";
+
+    
+    MathJax.typeset();
+});
+
+natmul.addEventListener('click', () => {
+    a = randomNumber(1,10);
+    b = randomNumber(1,10);
+
+    c = a*b;
+
+    natmul.innerHTML = "\\(" + a + " \\cdot " + b + "=\\)";
+    natmulL.innerHTML = "\\(\\," + c + "\\)";
+  
+    MathJax.typeset();
+});
+
+natdiv.addEventListener('click', () => {
+    a = randomNumber(0,10);
+    b = randomNumber(0,10);
+
+    c = a*b;
+
+    natdiv.innerHTML = "\\(" + c + " : " + b + "=\\)";
+
+    if (b != 0) 
+        natdivL.innerHTML = "\\(\\," + a + "\\)";
+    else
+        natdivL.innerHTML = " n.L.";
+ 
+    MathJax.typeset();
+});
+
+
+
+
+
+// Funktionen
+function randomNumber(min = 0, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+// Initialisierung der Felder
+natadd.click();
+natsub.click();
+natmul.click();
+natdiv.click();
+
+
+
+
+
+
+
+
+
