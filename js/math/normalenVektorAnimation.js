5a4fdfd202c3dba425d782c5653e033a4152aee9
Sebastian Heine <sebastian.heine@lk.brandenburg.de>
2026-01-06 07:53:55 +0100

    animation added

---
+++ js/math/normalenVektorAnimation.js
@@ -0,0 +1,354 @@
+
+
+Animation1();
+
+
+
+function Animation1() {
+
+    erw1.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 500,
+    });
+
+    erw2.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 1500,
+    });
+
+    // x-Koordinate
+    rah1.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 3000,
+    });
+    cal1.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 4000,
+    });
+    rah2.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 5000,
+    });
+    cal2.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 6000,
+    });
+    rah1.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 7000,
+    });
+    rah2.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 7000,
+    });
+
+    // y-Koordinate
+    rah3.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 10000,
+    });
+    cal3.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 11000,
+    });
+    rah4.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 12000,
+    });
+    cal4.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 13000,
+    });
+    rah3.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 14000,
+    });
+    rah4.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 14000,
+    });
+
+    // z-Koordinate
+    rah5.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 16000,
+    });
+    cal5.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 17000,
+    });
+    rah6.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 18000,
+    });
+    cal6.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 19000,
+    });
+    rah5.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 20000,
+    });
+    rah6.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 20000,
+    });
+
+    //
+    // Rechnung 2
+    //
+
+    erw4.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 500,
+    });
+
+    erw5.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 1500,
+    });
+
+    // x-Koordinate
+    rah11.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 3000,
+    });
+    cal11.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 4000,
+    });
+    rah22.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 5000,
+    });
+    cal22.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 6000,
+    });
+    rah11.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 7000,
+    });
+    rah22.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 7000,
+    });
+
+    // y-Koordinate
+    rah33.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 10000,
+    });
+    cal33.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 11000,
+    });
+    rah44.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 12000,
+    });
+    cal44.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 13000,
+    });
+    rah33.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 14000,
+    });
+    rah44.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 14000,
+    });
+
+    // z-Koordinate
+    rah55.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 16000,
+    });
+    cal55.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 17000,
+    });
+    rah66.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 18000,
+    });
+    cal66.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 19000,
+    });
+    rah55.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 20000,
+    });
+    rah66.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 20000,
+    });
+
+    // Letzter Vektor
+    erg1.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 22000,
+    });
+    erg2.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 23000,
+    });
+    erg3.animate([{ opacity: "0" }, { opacity: "1" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 24000,
+    });
+
+
+    // Unsichtbar machen :-)
+
+    erw1.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    erw2.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+
+    cal1.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    cal2.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    cal3.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    cal4.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    cal5.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    cal6.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+
+
+    erw4.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    erw5.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+
+    cal11.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    cal22.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    cal33.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    cal44.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    cal55.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+    cal66.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27000,
+    });
+
+    erg1.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27500,
+    });
+    erg2.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27500,
+    });
+    erg3.animate([{ opacity: "1" }, { opacity: "0" }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: 27500,
+    });
+
+
+
+    setTimeout(Animation1, 30000);
+
+}
